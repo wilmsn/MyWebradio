5656fed15af45c6bcda38543c8e09da4a1334d48
Norbert <mail@wilmie.de>
2023-08-30 17:59:52 +0200

    Initial noch nicht lauffähig

---
+++ src/myencoder.h
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+class MyEncoder {
+
+public:
+
+void begin();
+void begin(uint8_t startPos);
+void loop();
+void setPos(uint8_t newPos);
+void setLimits(uint8_t minLim, uint8_t maxLim);
+void setLev(uint8_t);
+bool newPos();
+uint8_t readPos();
+
+bool newLev();
+uint8_t readLev();
+
+bool isLongClicked(); 
+bool isShortClicked(); 
+
+bool isButtonDown();
+//bool isButtonClicked();
+
+private:
+uint8_t curPos = 25;
+bool posChanged = false;
+uint8_t curLev = 0;
+uint8_t maxLev = 1;
+bool levChanged = false;
+bool shortClicked = false;
+bool longClicked = false;
+unsigned long lastTimePressed = 0;
+
+};
